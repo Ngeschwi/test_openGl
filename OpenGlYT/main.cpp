648b1651c540bd1dd6d99d2f430be22a58c43e0f
Ngeschwi <nathan.geschwind@gmail.com>
2023-04-20 20:06:00 +0200

    try to implement class Shape

--- OpenGlYT/main.cpp
+++ OpenGlYT/main.cpp
@@ -12,9 +12,13 @@ GLFWwindow* window;
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-using namespace glm;
+
+//using namespace glm;
+
+#include "includes/Shapes.hpp"
 
 #include "common/shader.hpp"
+#include "common/texture.hpp"
 
 int main( void )
 {
@@ -69,54 +73,45 @@ int main( void )
     // Create and compile our GLSL program from the shaders
     GLuint programID = LoadShaders( "shaders/SimpleVertexShader.vert", "shaders/SimpleFragmentShader.frag" );
 
-
-    /*
-     * Matrix stuff
-     */
-
-    // Get a handle for our "MVP_cube" uniform
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-    // Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-    // Pour un rendu en perspective : (3D)
-    glm::mat4 Projection = glm::perspective(glm::radians(30.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-    // Or, for an ortho camera :
-    // pour un rendu en orthogonale : (2D) (une vue de dessus)
+    Shape triangle = Shape();
+    Shape cube = Shape();
+
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
     //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
 
     // Camera matrix
     glm::mat4 View = glm::lookAt(
-            glm::vec3(10, 9, 9), // Camera is at (4,3,3), in World Space
+            glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
             glm::vec3(0,0,0), // and looks at the origin
             glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
     );
 
-    // Translation matrix : move the object to the center of the world
-    glm::mat4 myTranslation_cube = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
-    // Rotation matrix : rotate the object around a chosen axis
-    glm::mat4 myRotation_cube = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    // Scale matrix : scale the object to 1/4 of its size
-    glm::mat4 myScale_cube = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+    triangle.setProjection(Projection);
+    cube.setProjection(Projection);
 
-    // Model matrix : an identity matrix (model will be at the origin) (Translation * Rotation * Scale)
-    glm::mat4 Model_cube = myTranslation_cube * myRotation_cube * myScale_cube;
+    triangle.setView(View);
+    cube.setView(View);
 
+    triangle.setTranslation(glm::vec3(0.0f, 0.0f, 0.0f));
+    triangle.setRotation(glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+    triangle.setScale(glm::vec3(1.0f, 1.0f, 1.0f));
 
-    glm::mat4 myTranslation_triangle = glm::translate(glm::mat4(1.0f), glm::vec3(4.0f, 0.0f, 0.0f));
-    glm::mat4 myRotation_triangle = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    glm::mat4 myScale_triangle = glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
+    cube.setTranslation(glm::vec3(0.0f, 0.0f, 0.0f));
+    cube.setRotation(glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+    cube.setScale(glm::vec3(1.0f, 1.0f, 1.0f));
 
-    glm::mat4 Model_triangle = myTranslation_triangle * myRotation_triangle * myScale_triangle;
+    triangle.setModel();
+    triangle.setMVP();
 
-    // Our ModelViewProjection : multiplication of our 3 matrices
-    glm::mat4 MVP_cube = Projection * View * Model_cube;
-    glm::mat4 MVP_triangle = Projection * View * Model_triangle;;
+    cube.setModel();
+    cube.setMVP();
 
+    cube.setTexture("uvTemplates/uvTemplate.DDS");
+    cube.setTextureID(programID, "myTextureSampler");
 
-
-    // Our vertices. Three consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-    // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-    static const GLfloat g_vertex_cube_buffer_data[] = {
+    const GLfloat g_vertex_cube_buffer_data[] = {
             -1.0f,-1.0f,-1.0f, // triangle 1 : begin
             -1.0f,-1.0f, 1.0f,
             -1.0f, 1.0f, 1.0f, // triangle 1 : end
@@ -154,56 +149,50 @@ int main( void )
             -1.0f, 1.0f, 1.0f,
             1.0f,-1.0f, 1.0f
     };
-    
-    // One color for each vertex. They were generated randomly.
-    static const GLfloat g_color_cube_buffer_data[] = {
-            0.583f,  0.771f,  0.014f,
-            0.609f,  0.115f,  0.436f,
-            0.327f,  0.483f,  0.844f,
-            0.822f,  0.569f,  0.201f,
-            0.435f,  0.602f,  0.223f,
-            0.310f,  0.747f,  0.185f,
-            0.597f,  0.770f,  0.761f,
-            0.559f,  0.436f,  0.730f,
-            0.359f,  0.583f,  0.152f,
-            0.483f,  0.596f,  0.789f,
-            0.559f,  0.861f,  0.639f,
-            0.195f,  0.548f,  0.859f,
-            0.014f,  0.184f,  0.576f,
-            0.771f,  0.328f,  0.970f,
-            0.406f,  0.615f,  0.116f,
-            0.676f,  0.977f,  0.133f,
-            0.971f,  0.572f,  0.833f,
-            0.140f,  0.616f,  0.489f,
-            0.997f,  0.513f,  0.064f,
-            0.945f,  0.719f,  0.592f,
-            0.543f,  0.021f,  0.978f,
-            0.279f,  0.317f,  0.505f,
-            0.167f,  0.620f,  0.077f,
-            0.347f,  0.857f,  0.137f,
-            0.055f,  0.953f,  0.042f,
-            0.714f,  0.505f,  0.345f,
-            0.783f,  0.290f,  0.734f,
-            0.722f,  0.645f,  0.174f,
-            0.302f,  0.455f,  0.848f,
-            0.225f,  0.587f,  0.040f,
-            0.517f,  0.713f,  0.338f,
-            0.053f,  0.959f,  0.120f,
-            0.393f,  0.621f,  0.362f,
-            0.673f,  0.211f,  0.457f,
-            0.820f,  0.883f,  0.371f,
-            0.982f,  0.099f,  0.879f
+
+   const GLfloat g_uv_buffer_data[] = {
+            0.000059f, 1.0f-0.000004f,
+            0.000103f, 1.0f-0.336048f,
+            0.335973f, 1.0f-0.335903f,
+            1.000023f, 1.0f-0.000013f,
+            0.667979f, 1.0f-0.335851f,
+            0.999958f, 1.0f-0.336064f,
+            0.667979f, 1.0f-0.335851f,
+            0.336024f, 1.0f-0.671877f,
+            0.667969f, 1.0f-0.671889f,
+            1.000023f, 1.0f-0.000013f,
+            0.668104f, 1.0f-0.000013f,
+            0.667979f, 1.0f-0.335851f,
+            0.000059f, 1.0f-0.000004f,
+            0.335973f, 1.0f-0.335903f,
+            0.336098f, 1.0f-0.000071f,
+            0.667979f, 1.0f-0.335851f,
+            0.335973f, 1.0f-0.335903f,
+            0.336024f, 1.0f-0.671877f,
+            1.000004f, 1.0f-0.671847f,
+            0.999958f, 1.0f-0.336064f,
+            0.667979f, 1.0f-0.335851f,
+            0.668104f, 1.0f-0.000013f,
+            0.335973f, 1.0f-0.335903f,
+            0.667979f, 1.0f-0.335851f,
+            0.335973f, 1.0f-0.335903f,
+            0.668104f, 1.0f-0.000013f,
+            0.336098f, 1.0f-0.000071f,
+            0.000103f, 1.0f-0.336048f,
+            0.000004f, 1.0f-0.671870f,
+            0.336024f, 1.0f-0.671877f,
+            0.000103f, 1.0f-0.336048f,
+            0.336024f, 1.0f-0.671877f,
+            0.335973f, 1.0f-0.335903f,
+            0.667969f, 1.0f-0.671889f,
+            1.000004f, 1.0f-0.671847f,
+            0.667979f, 1.0f-0.335851f
     };
 
-    GLuint vertexBufferCube;
-    glGenBuffers(1, &vertexBufferCube);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferCube);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_cube_buffer_data), g_vertex_cube_buffer_data, GL_STATIC_DRAW);
+    cube.setVertices(g_vertex_cube_buffer_data);
+    cube.setUv(g_uv_buffer_data);
 
-    GLuint colorBufferCube;
-    glGenBuffers(1, &colorBufferCube);
-    glBindBuffer(GL_ARRAY_BUFFER, colorBufferCube);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_cube_buffer_data), g_color_cube_buffer_data, GL_STATIC_DRAW);
+    cube.setBufferVBO();
 
     // another triangle but not in the cube
     static const GLfloat g_vertex_triangle_buffer_data[] = {
@@ -218,15 +207,10 @@ int main( void )
             0.327f, 0.483f, 0.844f
     };
 
-    GLuint vertexBufferTriangle;
-    glGenBuffers(1, &vertexBufferTriangle);
-    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferTriangle);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_triangle_buffer_data), g_vertex_triangle_buffer_data, GL_STATIC_DRAW);
+    triangle.setVertices(g_vertex_triangle_buffer_data);
+    triangle.setColors(g_color_triangle_buffer_data);
 
-    GLuint colorBufferTriangle;
-    glGenBuffers(1, &colorBufferTriangle);
-    glBindBuffer(GL_ARRAY_BUFFER, colorBufferTriangle);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_triangle_buffer_data), g_color_triangle_buffer_data, GL_STATIC_DRAW);
+    triangle.setBufferVBO();
 
     do{
         // Clear the screen
@@ -235,53 +219,9 @@ int main( void )
         // Use our shader
         glUseProgram(programID);
 
-        // Send our transformation to the currently bound shader,
-        // in the "MVP_cube" uniform
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP_cube[0][0]);
+        cube.draw(MatrixID, GL_TRIANGLES, 0, 12 * 3);
 
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferCube);
-        glVertexAttribPointer(
-                0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-                3,                  // size
-                GL_FLOAT,           // type
-                GL_FALSE,           // normalized?
-                0,                  // stride
-                (void*)0            // array buffer offset
-        );
-        // 2nd attribute buffer : colors
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colorBufferCube);
-        glVertexAttribPointer(
-                1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-                3,                                // size
-                GL_FLOAT,                         // type
-                GL_FALSE,                         // normalized?
-                0,                                // stride
-                (void*)0                          // array buffer offset
-        );
-        // Draw the triangle !
-        glDrawArrays(GL_TRIANGLES, 0, 12*3);
-
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
-
-
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP_triangle[0][0]);
-
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferTriangle);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-        // 2nd attribute buffer : colors
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colorBufferTriangle);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
+        //triangle.draw(MatrixID, GL_TRIANGLES, 0, 3);
 
         // Swap buffers
         glfwSwapBuffers(window);
@@ -292,10 +232,8 @@ int main( void )
            glfwWindowShouldClose(window) == 0 );
 
     // Cleanup VBO and shader
-    glDeleteBuffers(1, &vertexBufferCube);
-    glDeleteBuffers(1, &colorBufferCube);
-    glDeleteBuffers(1, &vertexBufferTriangle);
-    glDeleteBuffers(1, &colorBufferTriangle);
+    triangle.cleanUp();
+    cube.cleanUp();
     glDeleteProgram(programID);
     glDeleteVertexArrays(1, &VertexArrayID);
 
